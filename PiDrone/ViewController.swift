54b8e4c84c0fc06c831ac6819fcf6183d2663883
Andrew Ke <andrew@andrewke.org>
2015-05-02 18:33:57 -0700

    Initial Commit

---
+++ PiDrone/ViewController.swift
@@ -0,0 +1,182 @@
+  //
+//  ViewController.swift
+//  PiDrone
+//
+//  Created by Andrew Ke on 4/7/15.
+//  Copyright (c) 2015 Andrew Ke. All rights reserved.
+//
+//Ryan Chen
+//
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var GPSLabel: UILabel!
+    @IBOutlet weak var cameraShutterButton: UIButton!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var connectionLabel: UIButton!
+    @IBOutlet weak var dataTextField: UITextView!
+    @IBOutlet weak var serverSpeedLabel: UILabel!
+    @IBOutlet weak var armingButton: UIButton!
+    @IBOutlet weak var flightModeLabel: UIButton!
+    @IBOutlet weak var modePickerView: UIView!
+    @IBOutlet weak var videoStreamer: UIWebView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var safeModeLabel: UILabel!
+    @IBOutlet weak var homeButton: UIButton!
+    
+    var model:WebModel = WebModel()
+    var droneMarker = MKPointAnnotation()
+    var locationManager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.25, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+        roundView(self.GPSLabel)
+        roundView(self.cameraShutterButton)
+        roundView(self.videoButton)
+        roundView(connectionLabel)
+        roundView(dataTextField)
+        roundView(serverSpeedLabel)
+        roundView(armingButton)
+        roundView(homeButton)
+        
+        var url = NSURL (string: "https://news.google.com/")
+        if(model.safe == false){
+            self.safeModeLabel.hidden = true
+            url = NSURL (string: "http://\(model.ipadress):8080/stream_simple.html")
+        }
+        let requestObj = NSURLRequest(URL: url!)
+        //self.videoStreamer.scrollView.setZoomScale(2.0, animated: false)
+        videoStreamer.loadRequest(requestObj);
+        
+        let location = CLLocationCoordinate2D(
+            latitude: model.lat,
+            longitude: model.long
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        droneMarker = MKPointAnnotation()
+        droneMarker.coordinate = CLLocationCoordinate2DMake(model.lat, model.long)
+        droneMarker.title = "drone"
+        mapView.addAnnotation(droneMarker)
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        println("HELLO!")
+        let reuseId = "test"
+        
+        var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        if anView == nil {
+            anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            anView.canShowCallout = true
+        }
+        else {
+            anView.annotation = annotation
+        }
+        
+        
+        //Set annotation-specific properties **AFTER**
+        //the view is dequeued or created...
+        if(annotation.title! == "drone"){
+            println("changing!")
+            anView.image = UIImage(named:"miniquad.png")
+        }else{
+            //user dot
+            return nil
+        }
+        
+        return anView
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        //println("Hello!")
+        //mapView.setCenterCoordinate(userLocation.coordinate, animated: true);
+    }
+    
+    func mapViewDidFinishRenderingMap(mapView: MKMapView!, fullyRendered: Bool) {
+        println("Done!")
+    }
+    
+    @IBAction func centerAtHome() {
+        if(model.GPS == "FIX" || model.safe == true){
+            mapView.setCenterCoordinate(mapView.userLocation.location.coordinate, animated: true)
+        }else{
+            println("NO GPS, can't center on location")
+        }
+    }
+    
+    
+    @IBAction func switchDisplay(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            self.videoStreamer.hidden = false
+            self.mapView.hidden = true
+        case 1:
+            self.videoStreamer.hidden = true
+            self.mapView.hidden = false
+        default:
+            fatalError("Invalid Selection")
+        }
+    }
+    
+    func roundView(view:UIView){
+        view.layer.cornerRadius = 8.0
+        view.clipsToBounds = true
+    }
+    
+    func update() {
+        model.update()
+        self.dataTextField.text = "Altitude: \(model.altitude) m \n Speed: \(model.speed) m/s \n \n Yaw \(model.yaw)° \n Pitch \(model.pitch)° \n Roll \(model.roll)° \n"
+        self.flightModeLabel.setTitle(model.vehicleMode, forState: .Normal)
+        self.armingButton.setTitle(model.armed, forState: .Normal)
+        self.GPSLabel.text = "GPS: \(model.GPS)"
+        self.serverSpeedLabel.text = "\(model.serverSpeed) ms"
+        if(model.serverStatus){
+            self.connectionLabel.setTitle("\(model.ipadress) Connected", forState: .Normal)
+        }else{
+            self.connectionLabel.setTitle("\(model.ipadress) Disconnected", forState: .Normal)
+        }
+        
+        droneMarker.coordinate = CLLocationCoordinate2DMake(model.lat, model.long)
+        println("\(model.lat), \(model.long) ")
+        //self.mapView.removeAnnotation(droneMarker)
+        //self.mapView.addAnnotation(droneMarker)
+    }
+    
+    @IBAction func changeMode() {
+        self.modePickerView.hidden = false
+    }
+    
+
+    @IBAction func selectMode(sender: UIButton) {
+        self.modePickerView.hidden = true
+        model.setFlightMode(sender.currentTitle!)
+        //self.flightModeLabel.setTitle(sender.currentTitle, forState: .Normal)
+    }
+    
+    @IBAction func arm() {
+        if self.armingButton.currentTitle == "ARM"{
+            model.arm()
+        }else{
+            model.disarm()
+        }
+    }
+    
+    @IBAction func takePicture() {
+        model.takePicture()
+    }
+
+
+}
+
---
+++ PiDrone/WebModel.swift
@@ -0,0 +1,164 @@
+ //
+//  WebModel.swift
+//  PiDrone
+//
+//  Created by Andrew Ke on 4/9/15.
+//  Copyright (c) 2015 Andrew Ke. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+import UIKit
+
+class WebModel: NSObject{
+    
+    var safe = false
+    
+    var batteryVoltage = 0
+    var GPS = "NO FIX"
+    var signal = 100
+    
+    var yaw:Double = 0
+    var pitch:Double = 0
+    var roll:Double = 0
+    
+    var altitude:Double = 5
+    var speed:Double = 5
+    
+    var lat:Double = 37.411916
+    var long:Double = -121.995679
+    
+    var armed = "ARM"
+    
+    var vehicleMode = "STABILIZE"
+    
+    var serverStatus = false //true is Connected, False is not connected
+    var serverSpeed: Int = 0
+    
+    var ipadress = "10.0.0.9"
+    
+    override init(){
+        super.init()
+        //takePicture()
+    }
+    
+    //Make JSON request and update the stats accordingly
+    func update(){
+        var data = getJSON("http://\(ipadress):9090/JSON")
+        if data != nil{
+            let json = parseJSON(data!)
+            yaw = json["yaw"]!
+            pitch = json["pitch"]!
+            roll = json["roll"]!
+            
+            altitude = json["alt"]!
+            speed = json["groundspeed"]!
+            
+            speed = json["mode"]!
+            
+            lat = json["lat"]!
+            long = json["lon"]!
+            
+            //Decode integer to meaning of states (next three control flow things)
+            if json["armed"]! == 0{
+                armed = "ARM"
+            }
+            else{
+                armed = "DISARM"
+            }
+            
+            if json["gpsfix"]! > 2{
+                GPS = "FIX"
+            }
+            else{
+                GPS = "NO FIX"
+            }
+            
+            switch json["mode"]!{
+            case 0:
+                vehicleMode = "AUTO"
+            case 1:
+                vehicleMode = "STABILIZE"
+            case 2:
+                vehicleMode = "LOITER"
+            case 3:
+                vehicleMode = "ALT_HOLD"
+            default:
+                fatalError("NON STANDARD FLIGHT MODE USED!")
+            }
+        }
+    }
+    
+    //Function to get JSON using url from Stack Overflow. Had to add optionals
+    func getJSON(urlToRequest: String) -> NSData?{
+        var url = NSURL(string: urlToRequest)
+        var data:NSData? = nil
+        let start = NSDate()
+        if !safe{
+            data =  NSData(contentsOfURL: url!)
+        }
+        let end = NSDate();
+        serverSpeed = Int(round(1000*end.timeIntervalSinceDate(start)))
+        if data != nil{
+            serverStatus = true
+            return data
+        }else{
+            //println("Server down!")
+            serverStatus = false
+        }
+        return nil
+    }
+    
+    //parse json to dictionary. Changed type from NSDict to swift dict. Made the type of the returned Dictioanry explicit to prevent retreiving AnyObjects.
+    func parseJSON(inputData: NSData) -> Dictionary<String, Double>{
+        var error: NSError?
+        let jsonDic = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as! Dictionary<String, Double>;
+        
+        return jsonDic
+    }
+    
+    
+    //helper function to make request to url, returning nothing
+    func request(url: String){
+        if serverStatus == true {
+            //var url: NSURL = NSURL(string: url)!
+            //NSData(contentsOfURL: url)
+            let url = NSURL(string: url)
+            let request = NSURLRequest(URL: url!)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            }
+        }
+    }
+    
+    func takePicture(){
+        let url = NSURL(string: "http://\(ipadress):8080/?action=snapshot")
+        let request = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+            var picture = UIImage(data: data)
+            UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)
+            println("Picture Downloaded")
+        }
+        /*let url = NSURL(string: "http://10.0.0.15:8080/?action=snapshot")
+        let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+        let picture = UIImage(data: data!)
+        UIImageWriteToSavedPhotosAlbum(picture, nil, nil, nil)*/
+    }
+    
+    func setFlightMode(mode: NSString){
+        println("Flight mode changed to \(mode)")
+        request("http://\(ipadress):9090/iChangeMode?mode=\(mode)")
+    }
+    
+    //Arming and Disarming Functions
+    func arm(){
+        lat = 37
+        long = -121
+        println("Arming System")
+        request("http://\(ipadress):9090/iArm")
+    }
+    
+    func disarm(){
+        println("Disarming System")
+        request("http://\(ipadress):9090/iDisarm")
+    }
+}
